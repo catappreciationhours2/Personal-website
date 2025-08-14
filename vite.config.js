8954bc36f92b8eb9c1315017643927cd45b452fc
Noella Horo <nhoro@andrew.cmu.edu>
2025-08-14 18:45:59 +0530

    modified file path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const repoName = 'personal-website';
+const repoName = 'Personal-website';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/', 
