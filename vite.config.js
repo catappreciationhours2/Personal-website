96eb4164421982b1c29e4696f2d3521f64c76b3e
Noella Horo <nhoro@andrew.cmu.edu>
2025-08-14 22:51:39 +0530

    testing bug fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const repoName = 'personal-website';
 
 export default defineConfig({
   base: "/personal-website", 
