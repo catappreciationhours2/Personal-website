19e85ce96e11d992afcfc76b40656120e4ded922
Noella Horo <nhoro@andrew.cmu.edu>
2025-08-19 01:43:34 +0530

    commented out personal-website route in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig({
-  base: "/personal-website/", 
+  // base: process.env.NODE_ENV === 'production' ? '/personal-website/' : '/',
+  // base: "/personal-website/", 
   plugins: [react()],
 })
